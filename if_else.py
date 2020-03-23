5be778af239eda531e7a1be9ba7f637edf4f487f
Attawat <attawatkeroro@gmail.com>
2020-03-23 10:48:03 +0700

    basic if_else

--- if_else.py
+++ if_else.py
@@ -1,4 +1,16 @@
-a = int(input())
-b = int(input())
-sum = a/b
-print(sum)
+print('what area do you want')
+print('1.rectangle')
+print('2.triangle')
+n = input('what number = ')
+w = int(input('Weight = '))
+h = int(input('Height = '))
+def rectangle (w,h):
+    area = w * h
+    return area
+def triangle (w,h):
+    area = 0.5 * w * h
+    return area
+if n == "1":
+    print(rectangle(w,h))
+else:
+    print(triangle(w,h)) 
