966b47b7869bd9bbed42d098a8d9f6e6288cfefb
Attawat <attawatkeroro@gmail.com>
2020-03-28 12:40:46 +0700

    pratic

---
+++ googol.py
@@ -0,0 +1,31 @@
+
+def demo1():
+    # Google (googol) = 1e100
+    print('1' + ('0' * 100))     #str + str
+
+    # string.format
+    print('1{0}' .format('0' * 100))
+
+    # f-string (python 3.6++)
+    print(f'1{"0" * 100}')
+
+def demo2():
+    print('1', end = '')
+    for i in range(100):
+        print('0', end = '')
+
+def repeat(ch, times):
+    for i in range(times):
+        print(ch, end = '')
+
+def repeat2(ch, times):
+    s = ''
+    for i in range(times):
+        s += ch
+    return s
+
+
+# print('1', end = '')
+# repeat('0', 5)
+print(repeat2('0', 6))
+print('1'+repeat2('0', 100))
